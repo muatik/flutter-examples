06b08861eba88c70b09a85aae5a8a40c4558ba78
Mustafa Atik <muatik@gmail.com>
2020-09-16 21:29:57 +0200

    add location fetchAll method and use it in LocationDetail view

--- mejournal/lib/screens/location_detail/location_detail.dart
+++ mejournal/lib/screens/location_detail/location_detail.dart
@@ -1,25 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:mejournal/model/location.dart';
 import 'text_section.dart';
 import 'image_banner.dart';
-
 
 class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final locations = Location.fetchAll();
+    Location location = locations.first;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('hello')
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          ImageBanner("assets/images/bg1.jpeg"),
-          TextSection("Lean and Focused", "We cover the key concepts and widgets you need to learn to get productive. We skip the not-so-useful stuff."),
-          TextSection("End-to-End", "Learn how to write Flutter apps step by step. No over simplified examples, only production quality, realistic code."),
-          TextSection("Taught by a Flutter Developer", "All the tips and tricks learned from developing Flutter apps since 2017 are incorported into each course module."),
-        ]
-      )
-    );
+        appBar: AppBar(title: Text('hello')),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ImageBanner(location.imagePath),
+              ...location.facts.map((fact) => TextSection(fact.title, fact.description))
+            ]));
   }
 }
